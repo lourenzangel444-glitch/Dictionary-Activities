fab2fb0fe12844c521b67a5f76b8b941bb635838
Lourenz Angel A. Orcales <laorcales@paterostechnologicalcollege.edu.ph>
2026-04-22 14:48:47 +0800

    Update Dictionary Activities.py

--- Dictionary Activities.py
+++ Dictionary Activities.py
@@ -4,7 +4,7 @@ print("Dictionary Activities")
 print("=" * 50)
 
 # Activity 1: Fill in the Blanks
-print("\n🔹 Activity 1: Fill in the Blanks")
+print("\n Activity 1: Fill in the Blanks")
 student = {
    "name": "Ana",
    "age": 20,
@@ -13,20 +13,20 @@ student = {
 print(student["name"])
 
 # Activity 2: Add and Update
-print("\n🔹 Activity 2: Add and Update")
+print("\n Activity 2: Add and Update")
 student = {"name": "Ana", "age": 20}
 student["grade"] = 95
 student["age"] = 21   
 print(student)
 
 # Activity 3: Loop Through Dictionary
-print("\n🔹 Activity 3: Loop Through Dictionary")
+print("\n Activity 3: Loop Through Dictionary")
 car = {"brand": "Toyota", "model": "Corolla", "year": 2020}
 for key, value in car.items():
     print(f"{key} : {value}")
 
 # Activity 4: Mini Phonebook
-print("\n🔹 Activity 4: Mini Phonebook")
+print("\n Activity 4: Mini Phonebook")
 phonebook = {
     "Nicole": "09480673164",
     "Mico": "09943317430",
